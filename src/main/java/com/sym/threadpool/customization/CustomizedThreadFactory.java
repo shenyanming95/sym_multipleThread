46c3bc963c2368f3aae7d0c27303de07ef6e90d3
shenyanming <sym542569199@gmail.com>
2020-06-15 22:51:07 +0800

    线程池定制化

--- src/main/java/com/sym/threadpool/customization/CustomizedThreadFactory.java
+++ src/main/java/com/sym/threadpool/customization/CustomizedThreadFactory.java
@@ -1,27 +1,35 @@
 package com.sym.threadpool.customization;
 
+import java.util.Objects;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * 自定义线程工厂{@Link ThreadFactory},默认的线程池用的是{@link java.util.concurrent.Executors.DefaultThreadFactory}
+ * 定制化的线程工厂{@link ThreadFactory},
+ * 默认的线程池用的是{@link java.util.concurrent.Executors.DefaultThreadFactory}
  *
- * Created by shenym on 2019/9/6.
+ * @author shenyanming
+ * Created on 2019/9/6.
  */
 public class CustomizedThreadFactory implements ThreadFactory {
 
     private static AtomicInteger number = new AtomicInteger(1);
 
-    private String prefix = "sym-customized-theadPool:";
+    private static String prefix = "sym-customized-theadPool:";
 
-    private SecurityManager s = System.getSecurityManager();
+    private ThreadGroup group;
 
-    private ThreadGroup group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+    public CustomizedThreadFactory() {
+        SecurityManager securityManager = System.getSecurityManager();
+        group = Objects.nonNull(securityManager) ? securityManager.getThreadGroup() :
+                Thread.currentThread().getThreadGroup();
+    }
 
     @Override
     public Thread newThread(Runnable r) {
-        Thread t = new Thread(group,r,prefix+number.getAndIncrement(),0);
-        t.setPriority(Thread.NORM_PRIORITY);//设置线程优先级
+        Thread t = new Thread(group, r, prefix + number.getAndIncrement(), 0);
+        //设置线程优先级
+        t.setPriority(Thread.NORM_PRIORITY);
         return t;
     }
 }
--- src/main/java/com/sym/threadpool/customization/CustomizedThreadPool.java
+++ src/main/java/com/sym/threadpool/customization/CustomizedThreadPool.java
@@ -3,21 +3,22 @@ package com.sym.threadpool.customization;
 import java.util.concurrent.*;
 
 /**
- * 自定义线程池
+ * 定制化的线程池{@link ThreadPoolExecutor}
  *
- * Created by shenym on 2019/9/6.
+ * @author shenyanming
+ * Created on 2019/9/6.
  */
 public class CustomizedThreadPool {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         CustomizedThreadPool customizedThreadPool = new CustomizedThreadPool();
         ThreadPoolExecutor threadPool = customizedThreadPool.threadPoolExecutor();
-        for( int i = 0 ; i < 10 ; i++ ){
-            threadPool.execute(()->{
+        for (int i = 0; i < 10; i++) {
+            threadPool.execute(() -> {
                 // 线程都睡眠5s
                 try {
                     Thread.sleep(5000);
-                    System.out.println(Thread.currentThread().getName()+",执行完毕");
+                    System.out.println(Thread.currentThread().getName() + ",执行完毕");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -27,42 +28,31 @@ public class CustomizedThreadPool {
 
 
     /**
-     * 获取一个线程池
-     * @return
+     * 创建一个线程池
      */
-    public ThreadPoolExecutor threadPoolExecutor(){
-        return new ThreadPoolExecutor(
-                        1,
-                    2,
-                       20 , TimeUnit.SECONDS,
-                     blockingQueue(),
-                     threadFactory(),
-                     rejectedExecutionHandler()
-        );
+    public ThreadPoolExecutor threadPoolExecutor() {
+        return new ThreadPoolExecutor(1, 2, 20, TimeUnit.SECONDS, blockingQueue(), threadFactory(), rejectedExecutionHandler());
     }
 
 
     /**
-     * 获取自定义的线程工厂
-     * @return
+     * 创建自定义的线程工厂
      */
-    private ThreadFactory threadFactory(){
+    private ThreadFactory threadFactory() {
         return new CustomizedThreadFactory();
     }
 
     /**
-     * 获取自定义的拒绝策略
-     * @return
+     * 创建自定义的拒绝策略
      */
-    private RejectedExecutionHandler rejectedExecutionHandler(){
+    private RejectedExecutionHandler rejectedExecutionHandler() {
         return new CustomizedRejectedExecutionHandler();
     }
 
     /**
-     * 获取阻塞队列
-     * @return
+     * 创建阻塞队列
      */
-    private BlockingQueue<Runnable> blockingQueue(){
+    private BlockingQueue<Runnable> blockingQueue() {
         return new ArrayBlockingQueue<>(2);
     }
 }
